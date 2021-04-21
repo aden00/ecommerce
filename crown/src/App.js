b6e5039a071b021e14f1fc675222ef751e712453
ADEN Sham <adensham@ADENde-MacBook-Air.local>
2021-04-21 16:16:11 +0800

    nav bar added

--- crown/src/App.js
+++ crown/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import React from 'react'
 import HomePage from './pages/homepage/hompagecomponent'
 import ShopPage from "./pages/shop/shoppage";
-
+import Header from './components/header/header'
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact={true} path='/' component={HomePage}/>
         <Route exact path="/shop" component={ShopPage}/>
