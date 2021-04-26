6664dc8f995ce33dc432088eabd780fad01e5c4c
ADEN Sham <adensham@ADENde-MacBook-Air.local>
2021-04-26 22:15:15 +0800

    added shopping cart drop down

--- crown/src/components/header/header.js
+++ crown/src/components/header/header.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import './header.scss'
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import {auth} from '../../firebase/firebase.utils'
-
+import CartIcon from "../cart-icon/cart-icon";
+import CartDropDown from "../cart-dropdown/cart-dropdown";
 //redux 
 import { connect } from 'react-redux'
-const Header = ({currentUser}) => {
+const Header = ({currentUser,hidden}) => {
     return(
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -24,14 +25,15 @@ const Header = ({currentUser}) => {
                 :
                 <Link className='option' to='/signin'>
                     SIGN IN
-                </Link>
-            }
+                </Link>}
+                <CartIcon/>
             </div>
+            {hidden?null:<CartDropDown/>}
         </div>
     )
 }
-
-const mapStateToProps = state => ({
-    currentUser:state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) =>({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header)
